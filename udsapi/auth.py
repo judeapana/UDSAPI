5daa396b56b54d4fef58b07fd8bc7d9a8e99c2f3
Apana Jude <56265643+judeapana@users.noreply.github.com>
2020-01-03 08:16:27 +0000

    Update auth.py
    removed setup

--- udsapi/auth.py
+++ udsapi/auth.py
@@ -37,14 +37,3 @@ class Auth:
             raise SignInFailed('Login Failed, Please Check Credentials')
 
         return self.auth_sessions
-
-
-from udsapi.resultSet import *
-
-if __name__ == '__main__':
-    auth = Auth(username='fms/0051/17', password='apanayinime1')
-    auth.login()
-    course = CourseResultSet(auth.auth_sessions)
-    print(course.parser_data())
-    table = TableFinalResultSet(auth.auth_sessions)
-    print(table.dump_result(1))
